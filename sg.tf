d879dbafd8f2c1c63bc1b0bc7f94a368aa4995d7
englishtour0224 <englishtour0224@gmail.com>
2025-12-30 02:29:09 +0900

    feat(security): add seoul security group with peering rules

--- sg.tf
+++ sg.tf
@@ -24,3 +24,27 @@ resource "aws_vpc_security_group_ingress_rule" "primary_ingress_peer_icmp" {
   to_port           = -1
   ip_protocol       = "icmp"
 }
+
+# 2. Seoul Security Group (Secondary)
+resource "aws_security_group" "secondary_sg" {
+  provider    = aws.seoul
+  name        = "secondary-sg-prod"
+  description = "Security Group for Secondary VPC (SSM & Peering)"
+  vpc_id      = aws_vpc.secondary.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "secondary_egress_all" {
+  provider          = aws.seoul
+  security_group_id = aws_security_group.secondary_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "secondary_ingress_peer_icmp" {
+  provider          = aws.seoul
+  security_group_id = aws_security_group.secondary_sg.id
+  cidr_ipv4         = var.va_vpc_cidr
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+}
